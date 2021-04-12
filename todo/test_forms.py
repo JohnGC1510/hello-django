1ad64e034c7187e8c46c094e1884ac82837ee286
JohnGC1510 <johngc1510@gmail.com>
2021-04-12 16:57:52 +0000

    Initial Commit

---
+++ todo/test_forms.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from .forms import ItemForm
+
+
+class TestItemForm(TestCase):
+
+    def test_item_name_is_required(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())  # Ensure form is not valid
+        # Assert if name key exists in form errors
+        self.assertIn('name', form.errors.keys())
+        # Is error message the correct message
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_done_field_is_not_required(self):
+        form = ItemForm({'name': 'Test Item'})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = ItemForm()
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
